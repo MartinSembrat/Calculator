a7b36647ad8567f77587fcbb9e547d0281331d22
MartinSembrat <MartinSembrat@gmail.com>
2021-11-04 14:26:45 +0100

    updated sabeToFileTest to delete created file

--- src/main/java/calculator/controler/SaveToFile.java
+++ src/main/java/calculator/controler/SaveToFile.java
@@ -40,4 +40,9 @@ public class SaveToFile {
             return false;
         }
     }
+
+    public static void main(String[] args) {
+        for (int i=0; i<=5;i++)
+        saveToFile();
+    }
 }
--- src/test/java/calculator/controler/SaveToFileTest.java
+++ src/test/java/calculator/controler/SaveToFileTest.java
@@ -2,13 +2,37 @@ package calculator.controler;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import static calculator.controler.SaveToFile.formattedDate;
+import static calculator.controler.SaveToFile.counter;
 import static calculator.controler.SaveToFile.saveToFile;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SaveToFileTest {
 
     @Test
-    void saveToFileTest() {
+    void saveToFileTest() throws IOException {
         assertTrue(saveToFile());
+        byte b;
+        Path path = Paths.get(SaveToFile.path + "CalculationHistory" + formattedDate + counter + ".txt");
+        File  file = new File(SaveToFile.path + "CalculationHistory" + formattedDate + counter + ".txt");
+        RandomAccessFile f = new RandomAccessFile(file, "rw");
+        long length = f.length() - 1;
+        for (int i=0; i<=5;i++){
+            do {
+                length -= 1;
+                f.seek(length);
+                b = f.readByte();
+            } while(b != 10 && length>0);
+            f.setLength(length+1);
+        }
+
+        if (f.length()<2){
+            f.close();
+            Files.deleteIfExists(path);
+            System.out.println("deleted");}
     }
 }
